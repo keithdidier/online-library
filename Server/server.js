ab3199cab9d07551e53b70565676b85cc4e086ae
keithdidier <keith.b.didier@gmail.com>
2017-12-01 12:36:12 -0700

    books controller

---
+++ Server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+
+const books_controller = require('./controllers/books_controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db);
+})
+
+app.get('/api/books', books_controller.getBooks);
+
+const PORT = 3005;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
